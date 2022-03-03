c713f3a8044f65471c7fea25d03db080cfc30b78
Evgeny Zakharov <risenforces@gmail.com>
2022-03-03 19:39:59 +0300

    test: add some test cases

--- src/use-actual-callback.test.ts
+++ src/use-actual-callback.test.ts
@@ -1,5 +1,5 @@
 import { act, renderHook } from '@testing-library/react-hooks'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useLayoutEffect, useState } from 'react'
 import { useActualCallback } from './use-actual-callback'
 
 type Callback = (...args: unknown[]) => unknown
@@ -89,4 +89,48 @@ it('has the same reference after update', () => {
       previousReferences.simpleCallbackWithDeps
     )
   })
+})
+
+it('works in effects', async () => {
+  let checks = 0
+
+  const lastEquals = {
+    render: false,
+    effect: false,
+    layoutEffect: false,
+  }
+
+  const { result, waitFor } = renderHook(() => {
+    const [state, setState] = useState(1)
+
+    const actualCallback = useActualCallback(() => state)
+
+    const check = () => {
+      checks += 1
+      return actualCallback() === state
+    }
+
+    lastEquals.render = check()
+
+    useEffect(() => {
+      lastEquals.effect = check()
+    }, [state])
+
+    useLayoutEffect(() => {
+      lastEquals.layoutEffect = check()
+    }, [state])
+
+    return { setState }
+  })
+
+  const check = async (expectedChecks: number) => {
+    await waitFor(() => checks === expectedChecks)
+    expect(lastEquals.render).toBe(true)
+    expect(lastEquals.effect).toBe(true)
+    expect(lastEquals.layoutEffect).toBe(true)
+  }
+
+  await check(3)
+  act(() => result.current.setState(2))
+  await check(6)
 })
